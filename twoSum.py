1353a82e06f281ac4b87329c10933d6f17c4dd85
Zohraan <zohraanb@gmail.com>
2024-08-08 13:14:00 -0400

    solved twoSum

--- twoSum.py
+++ twoSum.py
@@ -1,1 +1,30 @@
-print("hello world")
+from typing import List
+
+
+# def twosum(nums, integer):
+#     for i, n in enumerate(nums):
+#         print(i)
+#         if n > integer:
+#             continue
+#         for j, m in enumerate(nums, start= i):
+#             if (n+m == integer):
+#                 print(i, "," , j)
+#                 return
+
+
+
+# twosum([2, 7 , 11, 15], 9)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap= {}
+        for i,n in enumerate(nums):
+            diff = target - n 
+            if (diff in hashMap):
+                return hashMap[diff], i
+            hashMap[n] = i 
+        return
+
+solution = Solution()
+
+print(solution.twoSum([2,7,11,15], 9))
